e0d5752463c924620000ab337b43c1dd4ba5c57c
Milln <brave.darkness@web.de>
2016-03-04 21:19:51 +0100

    Test.java was updated with new method

--- src/Test.java
+++ src/Test.java
@@ -2,11 +2,12 @@ public class Test {
 
     public static void main(String... args) {
         System.out.println("Hello World");
+        deineMutter();
     }
 
 
 
-    public void deineMutter(){
+    public static void deineMutter(){
         System.out.println("Ist fett xD");
     }
 }
