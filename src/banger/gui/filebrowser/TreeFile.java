3667f62b6af89382ccf7b92074d666f9744f8700
Milln <brave.darkness@web.de>
2016-03-22 12:51:04 +0100

    forgot 2 files

---
+++ src/banger/gui/filebrowser/TreeFile.java
@@ -0,0 +1,20 @@
+package banger.gui.filebrowser;
+
+import java.io.File;
+
+/**
+ * Created by Merlin on 22.03.2016.
+ */
+public class TreeFile {
+    private String name;
+    private File file;
+
+    public TreeFile(File f){
+        this.file = f;
+        this.name = f.getName();
+    }
+
+    public String toString() {
+        return name;
+    }
+}
