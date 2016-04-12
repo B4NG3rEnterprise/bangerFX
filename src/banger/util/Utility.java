96d5f40f04e38c88d718113e4b7db79a1cc20b8e
NickFried14 <nikisprivate@googlemail.com>
2016-04-12 20:16:20 +0200

    isDark() ficksed

--- src/banger/util/Utility.java
+++ src/banger/util/Utility.java
@@ -1,27 +1,15 @@
 package banger.util;
 
-import java.awt.*;
+import javafx.scene.paint.Color;
+
 import java.util.Locale;
 
 public class Utility {
 
     private Utility() {}
 
-    public static boolean isDark(String color){
-        String fontColor = color;
-        boolean isDark = false;
-
-        // remove hash character from string
-        String rawFontColor = fontColor.substring(1,fontColor.length());
-
-        // convert hex string to int
-        int rgb = Integer.parseInt(rawFontColor, 16);
-        Color c = new Color(rgb);
-        float[] hsb = Color.RGBtoHSB(c.getRed(), c.getGreen(), c.getBlue(), null);
-        float brightness = hsb[2];
-        if (brightness < 0.5) isDark = true;
-
-        return isDark;
+    public static boolean isDark(Color color){
+        return color.getBrightness()<0.75;
     }
 
     public static String withSuffix(long count) {
